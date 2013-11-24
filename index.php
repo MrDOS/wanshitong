5418dcef2034e4dda68cdfc0a455cd794d4eba8a
Samuel Coleman <samuel@seenet.ca>
2013-11-24 04:50:04 +0000

    Remove JSON routes. They're not going to work out with DI'd repositories.

--- index.php
+++ index.php
@@ -12,12 +12,17 @@
 
 $starttime = microtime(true);
 
+require_once 'config.php';
 require_once 'engine/wanshitong/Autoloader.php';
 spl_autoload_register(array(new \wanshitong\Autoloader(__DIR__ . DIRECTORY_SEPARATOR . 'engine'), 'load'));
 
 \wanshitong\TemplateManager::setDefaultTemplateManager(new \wanshitong\TemplateManager(__DIR__ . DIRECTORY_SEPARATOR . 'templates'));
 
-$routes = json_decode(file_get_contents('routes.json'), true);
-\wanshitong\Router::route($routes);
+$db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_HOST, DB_USER, DB_PASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+\wanshitong\Router::route(array(
+    '.*' => new \wanshitong\controllers\ErrorController()
+));
 
 printf("\n<!-- Page generated in %0.6fs. -->", microtime(true) - $starttime);
