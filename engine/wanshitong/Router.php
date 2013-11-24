e3c8aeab0a4776e1b2c8c075cbe11d56e17e048a
Samuel Coleman <samuel@seenet.ca>
2013-11-24 15:59:15 +0000

    Add support for callables to the router.

--- engine/wanshitong/Router.php
+++ engine/wanshitong/Router.php
@@ -14,21 +14,25 @@ class Router
     /**
      * Route a user request according to given mappings. Mappings should be an
      * associative array of route rules to classes extending
-     * \wanshitong\Controller.
+     * {@link \wanshitong\Controller}.
      *
      * A route rule is a regex describing a URL. For example, given a site at
      * "http://example.com/", this route rule:
      *
+     * <pre>
      *     /some_request
+     * </pre>
      *
-     * would match a request for "http://example.com/some_request". (The default
-     * route can be described by the rule "/".)
+     * would match a request for <copde>http://example.com/some_request</code>.
+     * (The default route can be described by the rule <code>/</code>.)
      *
      * Parameters can be matched by regex groups:
      *
+     * <pre>
      *     /some_request/([0-9]+)
+     * </pre>
      *
-     * would match a request for "http://example.com/some_request/17".
+     * would match the request <code>http://example.com/some_request/17</code>.
      *
      * Parameters described in such a fashion will be passed to the controller
      * as keys without values. To more sensibly retrieve the values, these
@@ -37,15 +41,19 @@ class Router
      * the controller, and the second being an array of parameter names as
      * strings. For example, to name the two parameters in the route:
      *
+     * <pre>
      *     /some_request/([A-Za-z]+)/([0-9]+)
+     * </pre>
      *
-     * as param1 and param2 for the controller ExampleController, the controller
-     * would be given as:
+     * as <code>param1</code> and <code>param2</code> for the controller
+     * <code>ExampleController</code>, the controller would be given as:
      *
-     * array(
-     *     'ExampleController',
-     *     array('param1', 'param2')
-     * )
+     * <pre>
+     *     array(
+     *         'ExampleController',
+     *         array('param1', 'param2')
+     *     )
+     * </pre>
      *
      * If there are more parameters matched than parameter names, or more
      * parameter names than matching parameters, the number of parameters passed
@@ -53,6 +61,13 @@ class Router
      *
      * If multiple route rules match the given URL, the first match met will be
      * used.
+     *
+     * The controller itself may be passed in one of three ways: it may be the
+     * string name of a Controller, an instance of a Controller, or a callable
+     * that returns an instance of a Contoller. This third method is useful when
+     * the construction of the controller or its dependencies is expensive. Note
+     * that the callable itself is <em>not</em> used as the route destination,
+     * but is simply used to provide the route destination.
      *
      * In addition to any parameters described in the route, GET values from the
      * query string will be passed. Named route parameters will take precidence,
@@ -107,7 +122,11 @@ class Router
                 }
 
                 $parameters = array_merge($_GET, $parameters);
-                return ($controller instanceof Controller) ? $controller : new $controller;
+                if ($controller instanceof Controller)
+                    return $controller;
+                elseif (is_callable($controller))
+                    return $controller();
+                return new $controller;
             }
         }
     }
